e8be369bc309c5298e3894c2978c55b0274993e9
unknown <kayanabdepbaki@gmail.com>
2026-04-24 02:51:37 +0300

    final

--- week3_project/main.py
+++ week3_project/main.py
@@ -18,6 +18,8 @@ load_dotenv()
 default_origins = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "http://127.0.0.1:8080",
+    "http://localhost:8080",
 ]
 
 frontend_origins = [
